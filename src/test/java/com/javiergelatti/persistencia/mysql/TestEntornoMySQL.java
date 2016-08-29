9995436cc2058d1b1db65c4bc15501b5506a380c
Facundo Javier Gelatti <javiergelatti@gmail.com>
2016-08-28 22:49:04 -0300

    Actualizar pruebas para considerar si el entorno se encuentra ejecutando mysql

---
+++ src/test/java/com/javiergelatti/persistencia/mysql/TestEntornoMySQL.java
@@ -0,0 +1,26 @@
+package com.javiergelatti.persistencia.mysql;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.javiergelatti.persistencia.mysql.BaseDeDatosMySQL.ErrorBD;
+import com.mysql.jdbc.exceptions.jdbc4.CommunicationsException;
+
+public class TestEntornoMySQL {
+	private static final String url = "jdbc:mysql://localhost/prueba";
+	private static final String usr = "prueba";
+	private static final String pwd = "prueba";
+	private static BaseDeDatosMySQL bd = new BaseDeDatosMySQL(url, usr, pwd);
+
+	@Test
+	public void laUnicaFormaDeQueNoSeEjecutenLasPruebasEsSiMySqlNoEstaIniciado() {
+		try {
+			bd.conectar();
+		} catch (ErrorBD e) {
+			Throwable causa = e.getCause();
+			assertTrue(causa instanceof CommunicationsException);
+		}
+	}
+
+}
